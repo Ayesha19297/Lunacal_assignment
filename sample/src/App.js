ed0ad443899e446419a19c1261135b142797afe4
ayesha19297 <ayesha19297@gmail.com>
2024-09-12 23:15:30 +0530

    initialization

---
+++ sample/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center bg-blue-500 text-white p-10">
+    <h1 className="text-3xl font-bold">Hello, Tailwind CSS is Working!</h1>
+  </div>
+  );
+}
+
+export default App;
---
+++ sample/tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This will include all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
